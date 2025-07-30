3520cb85e4d81fa8a9b36ac01f7d61dd354f1600
krtrivedi1975 <39838145+krtrivedi1975@users.noreply.github.com>
2025-07-30 09:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Ask Your AI Professor")
 
-openai.api_key = st.secrets["openai_api_key"]
+openai.api_key = st.secrets["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"]
 
 question = st.text_input("Ask a question:")
 
